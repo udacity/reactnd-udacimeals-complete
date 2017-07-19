e326c9569d89abc53da12ecd06f62f2c0c7a9389
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-07-18 23:07:43 -0600

    Create your first store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore } from 'redux'
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducer)
+
+console.log(store)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
