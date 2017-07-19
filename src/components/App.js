d0d4b8ade3dea46b7a3250ea196e0f862a399672
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-07-18 23:50:55 -0600

    All Togehter+

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
+import { addRecipe } from '../actions'
 
 class App extends Component {
+  state = {
+    calendar: null
+  }
+  componentDidMount () {
+    const { store } = this.props
+
+    store.subscribe(() => {
+      this.setState(() => ({
+        calendar: store.getState()
+      }))
+    })
+  }
+  submitFood = () => {
+    this.props.store.dispatch(addRecipe({
+      day: 'monday',
+      meal: 'breakfast',
+      recipe: {
+        label: this.input.value
+      },
+    }))
+
+    this.input.value = ''
+  }
   render() {
     return (
       <div>
-        Hello World
+        <input
+          type='text'
+          ref={(input) => this.input = input}
+          placeholder="Monday's Breakfast"
+        />
+        <button onClick={this.submitFood}>Submit</button>
+
+        <pre>
+          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -11,5 +11,8 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App store={store} />,
+  document.getElementById('root')
+)
 registerServiceWorker()
